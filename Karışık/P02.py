f0566479471946df9770a18b85b63b1914e4a801
mcecen <mustafacecen60@gmail.com>
2025-05-04 15:13:50 +0300

    04.05.2025-1

---
+++ Karışık/P02.py
@@ -0,0 +1,15 @@
+"""
+Write a program which can compute the factorial of a given numbers.
+The results should be printed in a comma-separated sequence on a single line.
+Suppose the following input is supplied to the program:
+8
+Then, the output should be:
+40320
+"""
+sayi = int(input("Sayi giriniz: "))
+def faktoriyel(sayi):
+    if sayi == 0:
+        return 1
+    return sayi*faktoriyel(sayi-1)
+
+print(faktoriyel(sayi))
