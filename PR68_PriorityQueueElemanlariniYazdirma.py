0a15a1586cabcd22152e2807d99fd3bc902baf7d
mcecen <mustafacecen60@gmail.com>
2025-06-15 22:48:35 +0300

    15.06.2025

--- PR68_PriorityQueueElemanlariniYazdirma.py
+++
@@ -1,9 +0,0 @@
-"""
- Bir Priority Queue  oluşruralım, elemanlarını ekleyim
-    priority queue yazdıralım.
-
-    priority queue elemanları: sari,yesil,mavi,kirmizi
-
-    Beklenen Çıktı:
-    Priority Queue elemanlari: [kirmizi, mavi, sari, yesil]
-"""
