4e1296e106770143adee68b97547b2a8bfda15bd
mcecen <mustafacecen60@gmail.com>
2025-06-15 18:14:09 +0300

    15.06.2025

--- raise.py
+++
@@ -1,8 +0,0 @@
-class Person:
-    def __init__(self, name, year):
-        if len(name) > 10:
-            raise Exception("name alanı fazla karakter içeriyor.")
-        else:
-            self.name = name
-
-p = Person("Mustafaaa", 1996)
