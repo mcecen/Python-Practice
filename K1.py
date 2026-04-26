83b24b4da278a86c7a4040d6c59ff0bfdc0f0a65
mcecen <mustafacecen60@gmail.com>
2026-04-27 01:21:14 +0300

    27.04.2026

--- K1.py
+++ K1.py
@@ -7,12 +7,17 @@ Aylık yol masrafı hesaplayan bir program yazmak istiyoruz. Elimizdeki veriler;
 4. İşten eve dönmek için kullandığımız vasıtanın ücreti 1.4 TL
 
 Aylık yol masrafı nedir?
-
 """
 
-calismaGunSayisi=22
-evdenIseUcret=1.5
-isdenEveUcret=1.4
+gün = 20
+gidiş_ücreti = 1.5
+dönüş_ücreti = 1.4
 
-masraf = calismaGunSayisi * (evdenIseUcret + isdenEveUcret)
-print("Aylık yol masrafınız:", masraf, "TL")
+masraf = gün * (gidiş_ücreti + dönüş_ücreti)
+
+print("-"*30)
+print("çalışılan gün sayısı\t:", gün)
+print("işe gidiş ücreti\t:", gidiş_ücreti)
+print("işten dönüş ücreti\t:", dönüş_ücreti)
+print("-"*30)
+print("AYLIK YOL MASRAFI\t:", masraf)
